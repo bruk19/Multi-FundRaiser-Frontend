3286f5c794199d52ee35a77b71572b0988fdd132
Bruk Teshome <brukteshome21@gmail.com>
2024-04-25 17:45:33 +0300

    import dependencies and files

--- multi-fundraiser-frontend/src/App.jsx
+++ multi-fundraiser-frontend/src/App.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { abi, contractAddress } from "./constants/fundRaiser";
+import { getWeb3, setupWeb3 } from "./web3";
 
 function App() {
 
