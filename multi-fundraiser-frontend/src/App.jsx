82fb06631e36079b31ee9e49b7a01faf1d3b11fa
Bruk Teshome <brukteshome21@gmail.com>
2024-04-25 17:54:28 +0300

    using useEffect web3 connection, retrieves the signer, creates a contract instance

--- multi-fundraiser-frontend/src/App.jsx
+++ multi-fundraiser-frontend/src/App.jsx
@@ -5,6 +5,22 @@ import { getWeb3, setupWeb3 } from "./web3";
 
 function App() {
 
+  useEffect(() => {
+    async function initialize() {
+      await setupWeb3();
+      const web3Instance = getWeb3();
+      console.log(web3Instance);
+
+      const signer = await web3Instance.getSigner();
+      console.log(signer)
+      const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+      setContract(contractInstance);
+
+      listOfFunds();
+    }
+    initialize();
+  }, []);
+
   return (
     <>
       <div>
